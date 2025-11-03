deddf4c6eb5450c1426a92966b663a1b32a7c56e
malenagrannerud <126866109+malenagrannerud@users.noreply.github.com>
2025-11-03 13:32:45 +0100

    0

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -16,8 +16,7 @@ const Tutorial = () => {
      
       <div className={pagePadding}>
         <p className={sectionSubheading}>
-          Välkommen! Den här appen är skapad för att hjälpa dig ta hand om din hälsa på ett enkelt sätt. 
-          Du bestämmer själv tempot - allt går att göra i din egen takt. Läs om hur du använder appen. Du kan när du vill läsa igen under "Mina sidor - Inställningar"
+        Läs om hur du använder appen. Du kan när du vill läsa igen under "Mina sidor - Inställningar"
         </p>
       </div>
 
