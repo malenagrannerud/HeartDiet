a0e6e4fbb4bdf161ece3ece5fbecbaf67440cc2a
malenagrannerud <126866109+malenagrannerud@users.noreply.github.com>
2025-11-20 15:47:54 +0100

    k

--- src/components/exCard.tsx
+++ src/components/exCard.tsx
@@ -10,7 +10,7 @@ interface ExampleCardProps {
 const ExampleCard = ({ goal, when, how, reminder }: ExampleCardProps) => {
   return (
     <div className="bg-pink-100 border border-pink-200 p-2 rounded-md">
-      <p><span className={exCardTextBold}>Mål: </span><span className="handwriting">{goal}</span></p>
+      <p><span className={exCardTextBold}>Mål: </span><span className="handwriting font-normal text-inherit">{goal}</span></p>
       <p><span className={exCardTextBold}>När: </span><span className="handwriting">{when}</span></p>
       <p><span className={exCardTextBold}>Hur: </span><span className="handwriting">{how}</span></p>
       <p><span className={exCardTextBold}>Påminnelse: </span><span className="handwriting">{reminder}</span></p>
