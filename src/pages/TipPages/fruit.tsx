1a49c66a509e8a1a3005e108778251462f1fd662
malenagrannerud <126866109+malenagrannerud@users.noreply.github.com>
2025-11-25 13:39:49 +0100

    k

--- src/pages/TipPages/fruit.tsx
+++ src/pages/TipPages/fruit.tsx
@@ -119,7 +119,10 @@ const FruitPage = () => {
         </div>
        
         <div>
-          <p className={bodyText}> <span className={bodyWhen}>Frukost</span> <span className={bodyHow}>Topping till gröt/musli/yoghurt/flingor</span> </p>
+          <p className={bodyText}> 
+            <span className={bodyWhen}>Frukost: </span> 
+            <span className={bodyHow}>Topping till gröt/musli/yoghurt/flingor</span> 
+          </p>
           <p className={bodyText}>Toppa med blåbär, hallon, banan eller äppelskivor. Addera kanel eller kardemumma.</p>
 
           <p><span className={bodyHow}>Smörgåspålägg</span></p>
