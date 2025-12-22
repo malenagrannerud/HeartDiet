688a93e315bcbbeea26c99ecf10ec6f6a104b17e
malenagrannerud <126866109+malenagrannerud@users.noreply.github.com>
2025-12-22 17:16:41 +0100

    Update fullkorn.tsx

--- src/pages/TipPages/fullkorn.tsx
+++ src/pages/TipPages/fullkorn.tsx
@@ -79,14 +79,15 @@ const FullkornPage = () => {
         )}
 
        <DottedList items={[
-  "Havregryn (100% fullkorn). En portion är ca 35 g",
-  <span key="bread">Vanligt bröd <ArrowExchange className="mx-2" /> Knäckebröd (100% fullkorn). 2 skivor är ca 24 g</span>,
-  "Musli (55% fullkorn)",
-  "Fullkornsbröd (20 - 70%) fullkorn",
-  <span key="pasta1">Pasta <ArrowExchange size={28} thickness="extra-thick" color="#22C55E" className="mx-2" /> Fullkornspasta (55% fullkorn). En portion är ca 40 g</span>,
-  <span key="pasta2">Pasta <ArrowExchange size={36} thickness="extra-thick" color="#00FF00" animate={true} className="mx-2" /> Mathavre (55% fullkorn). En portion är ca 40 g</span>,
-  <span key="flour">Vitt mjöl <ArrowExchange className="mx-2" /> Mjöl på fullkorn (100% fullkorn).</span>
-]} />
+        
+        "Havregryn (100% fullkorn). En portion är ca 35 g",
+        "Vanligt bröd <ArrowExchange /> Knäckebröd (100% fullkorn). 2 skivor är ca 24 g</span>,
+        "Musli (55% fullkorn)",
+        "Fullkornsbröd (20 - 70%) fullkorn",
+        "Pasta <ArrowExchange /> Fullkornspasta (55% fullkorn). En portion är ca 40 g</span>,
+        "Pasta <ArrowExchange />Mathavre (55% fullkorn). En portion är ca 40 g</span>,
+        "Vitt mjöl <ArrowExchange /> Mjöl på fullkorn (100% fullkorn).</span>
+      ]} />
 
         <div>
           <h2 className={sectionHeading2}>Varför minst 90 g om dagen?</h2>
